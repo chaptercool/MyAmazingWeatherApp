0eccc37e6cd8ae4b6011ccb73ab06afaa4d13920
Yaroslav <yr.hvrn15@gmail.com>
2025-04-04 16:53:16 +0200

    Init project
    Ui development start

---
+++ MyAmazingWeatherApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MyAmazingWeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new MainPage());
+            // window setting only for Desktop platforms (and for ease of testing)
+            window.Width = 432;
+            window.Height = 768;
+
+            return window;
+        }
+    }
+}
---
+++ MyAmazingWeatherApp/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MyAmazingWeatherApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Inter-Variable.ttf", "InterVariable");
+                fonts.AddFont("Inter-Italic-Variable.ttf", "InterItalicVariable");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
